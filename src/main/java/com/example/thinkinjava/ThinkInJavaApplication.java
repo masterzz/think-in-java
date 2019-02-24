3048b27d71eef973ff6ffd50694c58be5849787e
masterzz <545724476@qq.com>
2019-02-24 23:36:55 +0800

    新建项目

---
+++ src/main/java/com/example/thinkinjava/ThinkInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.thinkinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkInJavaApplication.class, args);
+    }
+
+}
